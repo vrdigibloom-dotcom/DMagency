c37932b541efd1bde66d8b164c4bb9f860bbd6c8
vrdigibloom-dotcom <vrdigibloom@gmail.com>
2026-06-11 20:11:58 +0530

    Add files via upload

---
+++ VR Digibloom/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const nav = document.getElementById("nav");
+  if (nav.style.display === "flex") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "flex";
+    nav.style.flexDirection = "column";
+  }
+}

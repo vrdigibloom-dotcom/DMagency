2fc6908ba11de8a30c2fe65375e1128f768c7aff
vrdigibloom-dotcom <vrdigibloom@gmail.com>
2026-08-08 18:44:23 +0530

    Update script.js

--- VR Digibloom/script.js
+++ VR Digibloom/script.js
@@ -1,9 +1,1 @@
-function toggleMenu() {
-  const nav = document.getElementById("nav");
-  if (nav.style.display === "flex") {
-    nav.style.display = "none";
-  } else {
-    nav.style.display = "flex";
-    nav.style.flexDirection = "column";
-  }
-}
+
